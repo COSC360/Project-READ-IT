2e1d544ce0f6ddd25ca4db1d58ff2fc33bf822bc
Lucadeverteuil <lucadeverteuil@gmail.com>
2023-03-16 20:10:07 -0700

    Testing to see if i can connect to server

--- backup_index.php
+++ backup_index.php
@@ -1,14 +1,4 @@
 
 <?php
 
-define('DBHOST', 'cosc360.ok.ubc.ca');
-define('DBNAME', 'db_81822173');
-define('DBUSER', '81822173');
-define('DBPASS', 'newpassword');
-session_start();
-$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
-$error = mysqli_connect_error();
-if($error != null){
-	exit("<p>Unable to connect to database</p>". $error);
-}
-?>
+echo "Hello World";
