5983be8590fb6c258d36d163583c54aa508ede1e
Lucadeverteuil <lucadeverteuil@gmail.com>
2023-03-16 21:31:16 -0700

    test server

--- backup_index.php
+++ backup_index.php
@@ -1,5 +1,15 @@
-
 <?php
+define('DBHOST', 'cosc360.ok.ubc.ca');
+define('DBNAME', 'db_28723147');
+define('DBUSER', '28723147');
+define('DBPASS', '28723147');
 
-echo "Hello World";
+			$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+			$error = mysqli_connect_error();
+			if($error != null){
+				exit("<p>Unable to connect to database</p>". $error);
+			} else {
+                echo "Connected to database";
+            }
+
 ?>
