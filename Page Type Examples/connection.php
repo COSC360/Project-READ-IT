676785869726ae684bfb6fcb23993acce52727ad
Fareeha <fareeha.stars@gmail.com>
2023-03-26 16:05:13 -0700

    Connecting to db

--- Page Type Examples/connection.php
+++ Page Type Examples/connection.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "project-read-it";
+$host = "cosc360.ok.ubc.ca";
+$username = "20489167";
+$password = "newpassword";
+$database = "db_20489167";
 // $database = "phpmyadmin";
 
 $connection = new mysqli($host, $username, $password, $database);
