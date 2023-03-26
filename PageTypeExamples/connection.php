3a15be41574538d9b56023e286a0622346b603ef
Fareeha <fareeha.stars@gmail.com>
2023-03-26 16:36:07 -0700

    Attempting again

--- PageTypeExamples/connection.php
+++ PageTypeExamples/connection.php
@@ -1,15 +1,15 @@
 <?php 
 
-$host = "localhost";
-$username = "20489167";
-$password = "newpassword";
-$database = "db_20489167";
+$host = 'cosc360.ok.ubc.ca';
+$username = '20489167';
+$password = 'newpassword';
+$database = 'db_20489167';
 // $database = "phpmyadmin";
 
 $connection = new mysqli($host, $username, $password, $database);
 
-if(mysqli_connect_error()) {
-    die("Server error");
+if(!$connection()) {
+    die(mysqli_connect_error());
 }
 
 ?>
