851eeaab6a4127445d84c2e15ea770cfc216f0c8
Fareeha <fareeha.stars@gmail.com>
2023-03-26 16:13:01 -0700

    Commenting out

--- backup_index.php
+++ backup_index.php
@@ -1,14 +1,14 @@
 <?php
-define('DBHOST', 'cosc360.ok.ubc.ca');
-define('DBNAME', 'db_20489167');
-define('DBUSER', '20489167');
-define('DBPASS', 'newpassword');
-			$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
-			$error = mysqli_connect_error();
-			if($error != null){
-				exit("<p>Unable to connect to database</p>". $error);
-			} else {
-                echo "Connected to database";
-            }
+// define('DBHOST', 'cosc360.ok.ubc.ca');
+// define('DBNAME', 'db_20489167');
+// define('DBUSER', '20489167');
+// define('DBPASS', 'newpassword');
+// 			$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+// 			$error = mysqli_connect_error();
+// 			if($error != null){
+// 				exit("<p>Unable to connect to database</p>". $error);
+// 			} else {
+//                 echo "Connected to database";
+//             }
 
 ?>
