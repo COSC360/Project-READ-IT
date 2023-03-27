03e55aabbe8136bfcb6b1ce80f5036c998f76a63
Lucadeverteuil <lucadeverteuil@gmail.com>
2023-03-27 12:31:39 -0700

    push

--- connection.php
+++ connection.php
@@ -4,16 +4,27 @@
 // $username = "root";
 // $password = "";
 // $database = "project-read-it";
+
 $host = "cosc360.ok.ubc.ca";
 $username = "81822173";
 $password = "newpassword";
-$database = "db_81822173";
-// $database = "phpmyadmin";
+$dbname = "db_81822173";
 
-$connection = new mysqli($host, $username, $password, $database);
+// // $database = "phpmyadmin";
 
-if(mysqli_connect_error()) {
-    die("Server error");
-}
+// $connection = new mysqli($host, $username, $password, $database);
+
+// if(mysqli_connect_error()) {
+//     die("Server error");
+// }
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    echo "Connected to $dbname at $host successfully.";
+    } catch (PDOException $pe) {
+    die ("Could not connect to the database $dbname :" . $pe->getMessage());
+    }
+
+
+
 
 ?>
