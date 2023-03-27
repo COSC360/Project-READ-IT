73c3689e68df1dd35a4a3e96d3c6d9a2ab1d57e1
Lucadeverteuil <lucadeverteuil@gmail.com>
2023-03-27 12:08:33 -0700

    testign to see if i can connect to server

--- connection.php
+++ connection.php
@@ -1,9 +1,13 @@
 <?php 
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "project-read-it";
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "project-read-it";
+$host = "cosc360.ok.ubc.ca";
+$username = "81822173";
+$password = "newpassword";
+$database = "db_81822173";
 // $database = "phpmyadmin";
 
 $connection = new mysqli($host, $username, $password, $database);
